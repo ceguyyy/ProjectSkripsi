2cb43a9a731d2fd09c0c82780b93992d0392304b
Christian Gunawan <cg404cgunawan@gmail.com>
2024-11-01 16:19:09 +0700

    Initial Commit

---
+++ ProjectSkripsi/ProjectSkripsiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSkripsiApp.swift
+//  ProjectSkripsi
+//
+//  Created by Christian Gunawan on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSkripsiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
